37c400208ddcf0972b42c67fb726cb174ea39939
Sarah Asad <sarah_asad@live.com>
2023-10-14 15:13:16 -0700

    la1 and lab2 complete

---
+++ Lab1a/EchoServer.java
@@ -0,0 +1,40 @@
+import java.net.*;
+import java.io.*;
+
+public class EchoServer {
+    public static void main( String args[] ) {
+		// validate the argument
+		if ( args.length != 1 ) {
+			System.err.println( "usage: java TcpServer port" );
+				return;
+		}
+
+		try {	
+			// create a server socket
+			ServerSocket server = new ServerSocket(Integer.parseInt(args[0]));
+			while(true) {
+				// accept a new connection from a new client
+				Socket client = server.accept();
+
+				// read a message
+				InputStream is = client.getInputStream();
+				byte[] data = new byte[1024];
+				int message = is.read(data);
+
+				// print it
+				System.out.println(new String(data, 0, message));
+
+				// just echo it back to the client
+				OutputStream os = client.getOutputStream();
+				os.write(data, 0, message); 
+				os.flush(); 
+
+				// close the connection
+				client.close();
+			
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+}
