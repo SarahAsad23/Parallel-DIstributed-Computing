4acdd5e64f07add33cc4293322e0349e3497c0fb
Sarah Asad <sarah_asad@live.com>
2023-11-05 20:48:58 -0800

    getting there

--- Program2/Heat2DO.java
+++ Program2/Heat2DO.java
@@ -4,17 +4,13 @@ public class Heat2D {
     private static double a = 1.0;  // heat speed
     private static double dt = 1.0; // time quantum
     private static double dd = 2.0; // change in system
-
-	//function used compute index 
-	public static int index (int p, int x, int y, int size){
-		return p * size * size + x * size + y;
-	}
     
     public static void main( String[] args ) {
 	// verify arguments
 	if ( args.length != 4 ) {
 	    System.out.
-		println( "usage: " + `"java Heat2D size max_time heat_time interval" );
+		println( "usage: " + 
+			 "java Heat2DO size max_time heat_time interval" );
 	    System.exit( -1 );
 	}
 
@@ -29,7 +25,7 @@ public class Heat2D {
 	for ( int p = 0; p < 2; p++ ) 
 	    for ( int x = 0; x < size; x++ )
 		for ( int y = 0; y < size; y++ )
-		    z[index(p, x, y, size)] = 0.0; // no heat or cold
+		    z[p][x][y] = 0.0; // no heat or cold
 	
 	// start a timer
 	Date startTime = new Date( );
@@ -40,20 +36,20 @@ public class Heat2D {
 	    
 	    // two left-most and two right-most columns are identical
 	    for ( int y = 0; y < size; y++ ) {
-		z[index(p, 0, y, size)] = z[index(p, 1, y, size)];
-		z[index(p, size - 1, y, size)] = z[index(p, size - 2, y, size)];
+		z[p][0][y] = z[p][1][y];
+		z[p][size - 1][y] = z[p][size - 2][y];
 	    }
 	    
 	    // two upper and lower rows are identical
 	    for ( int x = 0; x < size; x++ ) {
-			z[index(p, x, 0, size)] = z[index(p, x, 1, size)];
-		z[index(p, x, size - 1, size)] = z[index(p, x, size - 2, size)];
+		z[p][x][0] = z[p][x][1];
+		z[p][x][size - 1] = z[p][x][size - 2];
 	    }
 	    
 	    // keep heating the bottom until t < heat_time
 	    if ( t < heat_time ) {
 		for ( int x = size /3; x < size / 3 * 2; x++ )
-		z[index(p, x, 0, size)] = 19.0; // heat
+		    z[p][x][0] = 19.0; // heat
 	    }
 
 	    // display intermediate results
@@ -73,9 +69,9 @@ public class Heat2D {
 	    int p2 = (p + 1) % 2;
 	    for ( int x = 1; x < size - 1; x++ )
 		for ( int y = 1; y < size - 1; y++ )
-		z[index(p2, x, y, size)] = z[index(p, x, y, size)] + 
-			r * ( z[index(p, x + 1, y, size)] - 2 * z[index(p, x, y, size)] + z[index(p, x - 1, y, size)] ) +
-			r * ( z[index(p, x, y + 1, size)] - 2 * z[index(p, x, y, size)] + z[index(p, x, y - 1, size)] );
+		    z[p2][x][y] = z[p][x][y] + 
+			r * ( z[p][x+1][y] - 2 * z[p][x][y] + z[p][x-1][y] ) +
+			r * ( z[p][x][y+1] - 2 * z[p][x][y] + z[p][x][y-1] );
 	    
 	} // end of simulation
 	
--- Program2/Heat2D_mpi.java
+++ Program2/Heat2D_mpi.java
@@ -1,7 +1,7 @@
 import java.util.Date;
 import mpi.*; 
 
-public class Heat2D {
+public class Heat2D_mpi {
     private static double a = 1.0;  // heat speed
     private static double dt = 1.0; // time quantum
     private static double dd = 2.0; // change in system
@@ -11,15 +11,21 @@ public class Heat2D {
 		return p * size * size + x * size + y;
 	}
 
+	public static void display(int rank, String op, int toRank) {
+		System.out.println("Rank " + rank + ": " + op + " => " + toRank);
+	}
+
 	//add throws MPIException or else lots of errors 
     public static void main( String[] args ) throws MPIException{
 	
 	// verify arguments
 	if ( args.length != 4 ) {
 	    System.out.
-		println( "usage: " + "java Heat2D size max_time heat_time interval" );
+		println( "usage: " + "java Heat2D_mpi size max_time heat_time interval" );
 	    System.exit( -1 );
 	}
+
+	System.out.println("Starting.....................");
 
 	//initialize MPI 
 	MPI.Init(args); 
@@ -41,6 +47,8 @@ public class Heat2D {
 	int begin = (myRank < remainder) ? (stripe * myRank + myRank) : (stripe * myRank + remainder); 
 	int end = begin + stripe + ((myRank < remainder) ? 1 : 0) - 1; 
 
+	System.out.println("Slice Range: myRank=" + myRank + ", begin=" + begin + ", end=" + end); 
+
 	// create a 1D array representing 3d space 
 	double[] z = new double[2 * size * size];
 
@@ -51,12 +59,43 @@ public class Heat2D {
 	for ( int t = 0; t < max_time; t++ ) {
 	    int p = t % 2; // p = 0 or 1: indicates the phase
 
+
+	    // two left-most and two right-most columns are identical
+		// Only do it if the myRank is zero and you should be working on this slice.
+		if (begin == 0) {
+			for ( int y = 0; y < size; y++ ) {
+				z[index(p, 0, y, size)] = z[index(p, 1, y, size)];
+			}
+		}
+
+		if (begin <= (size - 2) && end <= size) {
+			for ( int y = 0; y < size; y++ ) {
+				z[index(p, size - 1, y, size)] = z[index(p, size - 2, y, size)];
+			}
+		}
+
+	    // two upper and lower rows are identical
+		// Only do it for the part of the row that this slice should operate on.
+	    for ( int x = begin; x < end; x++ ) {
+			z[index(p, x, 0, size)] = z[index(p, x, 1, size)];
+			z[index(p, x, size - 1, size)] = z[index(p, x, size - 2, size)];
+	    }
+
+	    // keep heating the bottom until t < heat_time
+		// Only do it for the assigned slice.
+	    if ( t < heat_time ) {
+			int startX = Math.max(size / 3, begin);
+			int endX = Math.min(2 * size / 3, end);
+			for ( int x = startX; x < endX; x++ ) {
+				z[index(p, x, 0, size)] = 19.0; // heat
+			}
+		}
+
 		// Calculate Eulers equation for local stripe 
-	    
-		for(int x = begin; x <= end; x++){
-			if(x == 0 || x == size - 1) continue; 
+		int p2 = (p + 1) % 2;
+		for(int x = begin + 1; x < end - 1; x++){
 			for(int y = 1; y < size - 1; y++){
-				z[index(p, x, y, size)] = z[index(p, x, y, size)] + 
+				z[index(p2, x, y, size)] = z[index(p, x, y, size)] + 
 				r * ( z[index(p, x + 1, y, size)] - 2 * z[index(p, x, y, size)] + z[index(p, x - 1, y, size)] ) +
 				r * ( z[index(p, x, y + 1, size)] - 2 * z[index(p, x, y, size)] + z[index(p, x, y - 1, size)] );
 			}
@@ -69,44 +108,81 @@ public class Heat2D {
 		// and then recieve all odd ranks will first receive and then send
 
 		// i am an even rank 
-		if(myRank % 2 == 0){
-			// send left and receive from right 
-			if(myRank > 0){
-				MPI.COMM_WORLD.Send(z, index(p, begin, 0, size), size, MPI.DOUBLE, myRank - 1, 0);
-				MPI.COMM_WORLD.Recv(z, index(p, begin - 1, 0, size), size, MPI.DOUBLE, myRank - 1, 0);
-			}
-			// send right and recieve from left 
-			if(myRank < mpiSize - 1){
-				MPI.COMM_WORLD.Send(z, index(p, end, 0, size), size, MPI.DOUBLE, myRank + 1, 0);
-				MPI.COMM_WORLD.Recv(z, index(p, end + 1, 0, size), size, MPI.DOUBLE, myRank + 1, 0);
+		int slice_size = (end - begin) * size;
+		int slice_width = (end - begin);
 
+	if (myRank % 2 == 0) {
+			// Compute the size of the slice.
+			//System.out.println("slice_size=" + slice_size + ", slice_width=" + slice_width);
+
+			// Sending to the worker on the left (if not the first one).
+			if (myRank > 0) {
+				//display(myRank, "Send", myRank - 1);
+				MPI.COMM_WORLD.Send(z, index(p, begin, 0, size), slice_size, MPI.DOUBLE, myRank - 1, 0);
+			}
+
+			if (myRank < mpiSize - 1) {
+				// Sending to the worker on the right (if not the last one).
+				//display(myRank, "Send", myRank + 1);
+				MPI.COMM_WORLD.Send(z, index(p, begin, 0, size), slice_size, MPI.DOUBLE, myRank + 1, 0);
+
+				// Receive from right (if not the last one.)
+				//display(myRank, "Recv", myRank + 1);
+				MPI.COMM_WORLD.Recv(z, index(p, end + 1, 0, size), slice_size, MPI.DOUBLE, myRank + 1, 0);
+			}
+
+			if (myRank > 0) {
+				//display(myRank, "Recv", myRank - 1);
+				MPI.COMM_WORLD.Recv(z, index(p, begin - slice_width, 0, size), slice_size, MPI.DOUBLE, myRank - 1, 0);
 			}
 		}
-		// i am an odd rank 
-		else{
-			// receive from right and send to left 
-			if(myRank < mpiSize - 1){
-				MPI.COMM_WORLD.Recv(z, index(p, end + 1, 0, size), size, MPI.DOUBLE, myRank + 1, 0);
-                MPI.COMM_WORLD.Send(z, index(p, end, 0, size), size, MPI.DOUBLE, myRank + 1, 0);
+		// i am an odd rank
+		else {
+
+			if (myRank < mpiSize - 1) {
+				// Receive from the worker on the right.
+				//display(myRank, "Recv", myRank + 1);
+				MPI.COMM_WORLD.Recv(z, index(p, end + 1, 0, size), slice_size, MPI.DOUBLE, myRank + 1, 0);
 			}
-			if(myRank > 0){
-				MPI.COMM_WORLD.Recv(z, index(p, begin - 1, 0, size), size, MPI.DOUBLE, myRank - 1, 0);
-                MPI.COMM_WORLD.Send(z, index(p, begin, 0, size), size, MPI.DOUBLE, myRank - 1, 0);
+
+			if (myRank > 0) {
+				// Recive from left.
+				//display(myRank, "Recv", myRank - 1);
+				MPI.COMM_WORLD.Recv(z, index(p, begin - slice_width, 0, size), slice_size, MPI.DOUBLE, myRank - 1, 0);
+
+				// Send to the left.
+				//display(myRank, "Send", myRank - 1);
+				MPI.COMM_WORLD.Send(z, index(p, begin, 0, size), slice_size, MPI.DOUBLE, myRank - 1, 0);
 			}
-		}
-	    
-	    // display intermediate results
-	    if ( interval != 0 && ( t % interval == 0 || t == max_time - 1 ) ) {
-			System.out.println( "time = " + t );
-			for ( int y = 0; y < size; y++ ) {
-				for ( int x = 0; x < size; x++ )
-				System.out.print( (int)( Math.floor(z[index(p, x, y, size)] / 2) ) + " " );
-				System.out.println( );
+
+			if (myRank < mpiSize - 2) {
+				// Send to the right.
+				//display(myRank, "Send", myRank + 1);
+				MPI.COMM_WORLD.Send(z, index(p, begin, 0, size), slice_size, MPI.DOUBLE, myRank + 1, 0);
 			}
+		}		
+	
+
+	// display intermediate results
+	//System.out.println("rank = " + myRank + ", Displaying Results v2");
+
+	if (myRank == 0) {
+		for ( int y = 0; y < size; y++ ) {
+			for ( int x = 0; x < size; x++ ) {
+				System.out.print((int)( Math.floor(z[index(p, x, y, size)] / 2) ) + " " );
+				//System.out.print(z[index(0, x, y, size)] + " " );
+				//System.out.print( (int)( Math.floor(z[p][x][y] / 2) ) 		+ " " );
+	  		}	
+			
 			System.out.println( );
-	    }
+		}
+		System.out.println( );
+	}
+
+		//System.out.println("rank = " + myRank + ", time = " + t );
 	} // end of simulation
 	
+
 	// finish the timer
 	Date endTime = new Date( );
 	System.out.println( "Elapsed time = " + ( endTime.getTime( ) - startTime.getTime( ) ) );	
