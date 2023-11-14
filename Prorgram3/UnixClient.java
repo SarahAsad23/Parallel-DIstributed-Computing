4a2e84680d61e4f42a91985bce34152663cacef3
Sarah Asad <sarah_asad@live.com>
2023-11-14 15:45:16 -0800

    WIP

--- Prorgram3/UnixClient.java
+++ Prorgram3/UnixClient.java
@@ -5,23 +5,51 @@
 */
 
 import java.rmi.*;
+import java.util.Date;
 
 public static void main (String args[]) {
-    try{
-        //look for the server instance client wants to access
-        ServerInterface server = ( ServerInterface )
-        Naming.lookup( “rmi://serverIp:serverPort/symbolic_name” );
-    } catch (Exception e){
-        e.printStackTrace(); 
+    //java UnixClient P/C port NumberOfServers ListOfServers 
+    //NumberOfUnixCommands ListOfUnixCommands
+    
+    if(args.length != 6){
+        System.out.println("Usage: java UnixClient P/C port NumberOfServers 
+            ListOfServers NumberOfUnixCommands ListOfUnixCommands")
         System.exit(-1); 
     }
 
-    // then we want to call server execute function 
-    // this is where the return value will be stored 
-    Vector<String> returnValue = new Vector<>(); 
+    String pOrC = args[0];                                             // P or C
+    int port = Integer.parseInt(args[1]);                              // Port 
+    int numServers = Integer.parseInt(args[2]);                        // Number of Servers 
+    String[] serverList = Arrays.copyOfRange(args, 3, 3 + numServers); // List of Servers
+    int numCommands = Integer.parseInt(3 + numServers + 1);            // Number of Commands 
+    String[] commands = Arrays.copyOfRange(args, 3 + numServers + 1, 
+        3 + numServers + 1 + numCommands);                             // List of Commands 
 
-    try {
-        returnValue = server.execute();
-    } catch (Exception e) {}
+    //start the timer 
+    Date startTime = new Date();
 
+    for(String s : serverList){
+        try{
+            //look for the server instance client wants to access
+            ServerInterface server = (ServerInterface)
+                Naming.lookup("rmi://" + s + ":" + port + "/UnixServer");
+        } catch (Exception e){
+            e.printStackTrace(); 
+            System.exit(-1); 
+        }
+
+        // then we want to call server execute function 
+        // this is where the return value will be stored 
+        Vector<String> returnValue = new Vector<>(); 
+
+        try {
+            returnValue = server.execute();
+        } catch (Exception e) {}
+    }
+    
+    //end the timer 
+    Date endTime = new Date();
+
+    //print results
+    System.out.println( "Elapsed time = " + ( endTime.getTime() - startTime.getTime()));
 }
