7e9779d5ac7dee0c27702ce79c31a5fd301854be
Sarah Asad <sarah_asad@live.com>
2023-11-13 20:03:26 -0800

    lab 3b done

---
+++ Lab3b/MyAgent.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import UWAgent.*;
+
+public class MyAgent extends UWAgent implements Serializable {
+    private String destination = null;
+   
+    public MyAgent( String[] args ) {
+        System.out.println( "Injected" );
+        destination = args[0];
+    }
+
+    public MyAgent( ) { 
+        System.out.println( "Injected" );
+        destination = "localhost";
+    }
+
+    public void init( ) {
+        System.out.println( "I'll hop to " + destination );
+        String[] args = new String[1];
+        args[0] = "hello";
+        System.out.print.out("hop"); 
+        hop( "cssmpi2h", "step", args );
+    }
+
+    public void step(){
+        System.out.print.out("step"); 
+        hop( "cssmpi3h", "jump", args );
+    }
+
+    public void jump(){
+        System.out.print.out("jump");
+    }
+
+    public void func( String[] args ) {
+	    System.out.println( args[0] );
+    }
+}
