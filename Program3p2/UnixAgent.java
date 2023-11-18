09c71267608ef95eebe241f0a992f6467ce2a7ed
Sarah Asad <sarah_asad@live.com>
2023-11-17 21:49:52 -0800

    code almost complete

--- Program3p2/UnixAgent.java
+++ Program3p2/UnixAgent.java
@@ -1,41 +1,87 @@
+/*
+* Sarah Asad 
+* CSS 434: Parallel & Distributed Computing 
+*/
+
 import java.io.*;
 import UWAgent.*;
 
 public class UnixAgent extends UWAgent implements Serializable {
-    private String destination = null;
     
-    public AnAgent( String[] args ) {
-	    // Should have at least 6 arguments 
-        if(args.length < 6){
+    // get the localhost so we know where to hop back 
+    // when we are done executing all commands 
+    String localHost = InetAddress.getLocalHost(); 
+    String me = localHost.getHostAddress();   
+
+    // this is where the return value will be stored
+    Vector<String> output = new Vector<String>( );  
+
+    // keep track of the number of hops so we know when to print 
+    int numHops = 0; 
+    
+    public UnixAgent( String[] args ) {
+	    
+        // Should have at least 6 arguments 
+        if(args.length < 5){
             System.out.println("Usage: java UnixClient P/C port 
             NumberOfServers ListOfServers NumberOfUnixCommands ListOfUnixCommands");
             System.exit(-1); 
         }
 
         // Get all the arguments from the command line
-        String pOrC = args[0];                                              // P or C
-        int port = Integer.parseInt(args[1]);                               // Port 
-        int numServers = Integer.parseInt(args[2]);                         // Number of Servers 
-        String[] serverList = Arrays.copyOfRange(args, 3, 3 + numServers);  // List of Servers
-        int numCommands = Integer.parseInt(args[3 + numServers]);           // Number of Commands 
-        String[] commands = Arrays.copyOfRange(args, 3 + numServers + 1, 
-            3 + numServers + 1 + numCommands);
+        String pOrC = args[0];                                              // P or C    
+        int numServers = Integer.parseInt(args[1]);                         // Number of Servers 
+        String[] serverList = Arrays.copyOfRange(args, 2, 2 + numServers);  // List of Servers
+        int numCommands = Integer.parseInt(args[2 + numServers]);           // Number of Commands 
+        String[] commands = Arrays.copyOfRange(args, 2 + numServers + 1,    // List of Commands
+            2 + numServers + 1 + numCommands);
     
-
-    
-    System.out.println( "Injected" );
-	
-    destination = args[0];
-    }
+        System.out.println( "Injected" );
+	}
 
     public void init( ) {
-	System.out.println( "I'll hop to " + destination );
-	String[] args = new String[1];
-	args[0] = "hello";
-	hop( destination, "func", args );
+        if(numHops == serverList.size()){
+            hop(me, "printOut", null)
+        }
+
+        for(String s : serverList){
+            System.out.println( "I'll hop to " + s );
+            hop(s, "runCommands", null)
+            numHops++;
+        }
     }
 
-    public void func( String[] args ) {
-	System.out.println( args[0] );
+    // print out the results
+    public void printOut(){
+        // print the contents of the array
+        if(porC = "P"){
+            for(String s : output){
+                System.out.println(s);
+            }
+        }
+        // print the size of the array 
+        else if(PorC == "C"){
+            System.out.println(output.size());
+        }
+    }
+
+    // run the command on each server 
+    public void runCommands(){
+        for(String s : commands){
+            try{
+                // execute the command 
+			    Process process = Runtime.getRuntime().exec(c);
+                // store the result
+                InputStream input = process.getInputStream();
+			    BufferedReader bufferedInput = new BufferedReader(new InputStreamReader(input));
+			    while ((line = bufferedInput.readLine()) != null) {
+			        output.addElement(line);
+			    }
+            }
+            catch(IOException e){
+                e.printStackTrace(); 
+                System.exit(-1); 
+            } 
+        }
     }
 }
