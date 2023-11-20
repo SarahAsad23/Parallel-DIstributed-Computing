1e0d045ce9dafe9ff5bb17af02bd15f5373b8bc7
Sarah Asad <sarah_asad@live.com>
2023-11-19 17:26:03 -0800

    code works!!

--- Program3p2/UnixAgent.java
+++ Program3p2/UnixAgent.java
@@ -19,6 +19,8 @@ public class UnixAgent extends UWAgent implements Serializable {
     // keep track of the number of hops so we know when to print 
     int numHops = 0; 
 
+    String me = ""; // origin 
+
     // args
     String pOrC;
     String[] serverList;
@@ -49,32 +51,22 @@ public class UnixAgent extends UWAgent implements Serializable {
 
         // get the localhost so we know where to hop back 
         // when we are done executing all commands 
-        String me = ""; 
-
         try{
             me = InetAddress.getLocalHost().getHostAddress();
         } catch(UnknownHostException e){}
 
-        System.out.println("i am: " + me);
-
         // start the timer
-        Date startTime = new Date();
+        //Date startTime = new Date();
 
-        for(String s : serverList){
-            System.out.println("I'll hop to " + s);
-            hop(s, "runCommands", commands);
-            numHops++;
-        }
-
-         if(numHops == serverList.length){
-            String[] outputArray = output.toArray(new String[output.size()]);
-            hop(me, "printOut", outputArray);
-        }
-
+        // hop to the first server in the list 
+        System.out.println("I'll hop to " + serverList[0]);
+        numHops++; 
+        hop(serverList[0], "runCommands", commands);
+            
         //end the timer 
-        Date endTime = new Date();
+        //Date endTime = new Date();
         // print elapsed time 
-        System.out.println( "Elapsed time = " + ( endTime.getTime() - startTime.getTime()));
+        //System.out.println( "Elapsed time = " + ( endTime.getTime() - startTime.getTime()));
     }
 
     // print out the results
@@ -84,16 +76,16 @@ public class UnixAgent extends UWAgent implements Serializable {
         // print the contents of the array
         if(pOrC.equals("P")){
             for(String s : arr){
-                System.out.println("I AM P");
+                //System.out.println("I AM P");
                 System.out.println(s);
-                System.out.println("i am done printing P");
+                //System.out.println("i am done printing P");
             }
         }
         // print the size of the array 
         else if(pOrC.equals("C")){
-            System.out.println("I AM C"); 
+            //System.out.println("I AM C"); 
             System.out.println(arr.length);
-            System.out.println("i am done printing C");
+            //System.out.println("i am done printing C");
         }
     }
 
@@ -119,5 +111,21 @@ public class UnixAgent extends UWAgent implements Serializable {
             e.printStackTrace(); 
             System.exit(-1); 
         } 
+
+        System.out.println("Server List Length: " + serverList.length + " NumHops: " + numHops); 
+
+        if(numHops != serverList.length){
+            System.out.println("I'll hop to " + serverList[numHops]); 
+            numHops++;
+            int index = numHops - 1; 
+            hop(serverList[index], "runCommands", commands);
+            
+            
+        }
+        else if(numHops == serverList.length){
+            System.out.println("I'll hop back to origin " + me);
+            String[] outputArray = output.toArray(new String[output.size()]);
+            hop(me, "printOut", outputArray);
+        }
     }
 }
