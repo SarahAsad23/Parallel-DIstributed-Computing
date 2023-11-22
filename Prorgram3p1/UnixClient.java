091ce211532950490d63e740ab1c211f4083f68b
Sarah Asad <sarah_asad@live.com>
2023-11-21 23:02:04 -0800

    done

--- Prorgram3p1/UnixClient.java
+++ Prorgram3p1/UnixClient.java
@@ -3,20 +3,26 @@
 * CSS 434: Parallel & Distributed Computing 
 */
 
+
+import java.util.*;
 import java.rmi.*;
-import java.util.Date;
-import java.util.Vector;
-import java.util.Arrays;
+import java.rmi.server.UnicastRemoteObject;
 
-public class UnixClient{
+public class UnixClient extends UnicastRemoteObject {
+    private boolean print = false; // whether or not we should print contents 
+    private int count = 0; //store the size 
+    private Vector<String> all = new Vector<>(); // all values are stored here
 
-    public static void main(String args[]) {
-        
-        // Should have at least 6 arguments 
-        if(args.length < 6){
-            System.out.println("Usage: java UnixClient P/C port 
-            NumberOfServers ListOfServers NumberOfUnixCommands ListOfUnixCommands");
-            System.exit(-1); 
+    // coppied from UnixServer to see if P (otherwise C)
+    UnixClient(String print) throws RemoteException {
+        this.print = print.startsWith("P");
+    }
+
+    public static void main(String[] args) throws RemoteException {
+        // Should have at least 6 arguments
+        if (args.length < 6) {
+            System.out.println("Usage: java UnixClient P/C port NumberOfServers ListOfServers NumberOfUnixCommands ListOfUnixCommands");
+            System.exit(-1);
         }
 
         String pOrC = args[0];                                              // P or C
@@ -26,51 +32,70 @@ public class UnixClient{
         int numCommands = Integer.parseInt(args[3 + numServers]);           // Number of Commands 
         String[] commands = Arrays.copyOfRange(args, 3 + numServers + 1, 
             3 + numServers + 1 + numCommands);                              // List of Commands
-     
-        // start the timer 
-        Date startTime = new Date();
 
-        // this is where the return value will be stored
-        Vector<String> returnValue = new Vector<String>();
+        UnixClient myClient = new UnixClient(pOrC); // initialize client 
 
-        ServerInterface server = null;
+        Date startTime = new Date(); // start the timer 
 
-        for(String s : serverList){
-            try{
+        ServerInterface[] servers = new ServerInterface[numServers]; 
+
+        // establish a connection to each server 
+        for (int i = 0; i < numServers; i++) { 
+            try {
                 // look for the server instance client wants to access
-                server = (ServerInterface) Naming.lookup("rmi://" + s + ":" + port + "/unixserver");
-            } catch (Exception e){
-                e.printStackTrace(); 
-                System.exit(-1); 
-            }
-
-            // iterate through commands and run at each server 
-            for(String c : commands){
-                try {
-                    System.out.println(s + " command: " + c); 
-                    
-                    // call server execute function
-                    returnValue = server.execute(c);
-
-                    // print results 
-                    if(pOrC.equals("P")){
-                        for(String t : returnValue){
-                            System.out.println(t); 
-                        }
-                    }
-                    else if(pOrC.equals("C")){
-                        System.out.println(returnValue.size()); 
-                    }
-
-               } catch (Exception e) {}
+                servers[i] = (ServerInterface) Naming.lookup("rmi://" + serverList[i] + ":" + port + "/unixserver");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(-1);
             }
         }
 
-        // end the timer 
-        Date endTime = new Date();
+        // execute the commands 
+        myClient.executeCommands(servers, commands);
 
-        // print elapsed time 
-        System.out.println( "Elapsed time = " + ( endTime.getTime() - startTime.getTime()));
+        //if P then we print the contents of the vector 
+        if (myClient.print) {
+            for (String s : myClient.all) {
+                System.out.println(s);
+            }
+        }
+        // if C then we print the size of the vector 
+        else{
+            System.out.println("Count: " + myClient.count);
+        }
+
+        Date endTime = new Date(); // end the timer 
+        // compute and print the elapsed time
+        System.out.println("Elapsed time = " + (endTime.getTime() - startTime.getTime()));
+
+        System.exit(0); // force exit otherwise stays stuck 
+    }
+
+    // Executes all commands at each server 
+    private void executeCommands(ServerInterface[] servers, String[] commands) {
+        // this is where the return value will be stored
+        Vector<String> returnValue;
+
+        // iterate through server list 
+        for (ServerInterface server : servers) {
+            // and commands 
+            for (String command : commands) {
+                try {
+                    // call server execute function
+                    returnValue = server.execute(command);
+                    //if P 
+                    if (print) {
+                        // then add all the values to all vector for later printing 
+                        all.add("running command " + command);
+                        all.addAll(returnValue);
+                    }
+                    // compute the size 
+                    count += returnValue.size();
+                } catch (Exception e) {
+                    e.printStackTrace(); 
+                }
+            }
+            all.add(""); // separates different server outputs
+        }
     }
 }
-
